fc515a5c2f10463a2a829cfdeeba247496ee09f7
Jaime <santos.jaimemendes91@gmail.com>
2023-04-29 10:48:11 -0300

    separada a tela de login e implementado o componente de layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
-import { Component, AfterViewInit } from '@angular/core';
-import * as $ from 'jquery';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -8,25 +7,4 @@ import * as $ from 'jquery';
 })
 export class AppComponent {
   title = 'clientes-app';
-
-  
-  ngAfterViewInit() {
-    (function($) {
-        "use strict";
-    
-        // Add active state to sidbar nav links
-        var path = window.location.href; // because the 'href' property of the DOM element is the absolute path
-        $("#layoutSidenav_nav .sb-sidenav a.nav-link").each(function() {
-            if (this instanceof HTMLAnchorElement && this.href === path) {
-                $(this).addClass("active");
-            }
-        });
-    
-        // Toggle the side navigation
-        $("#sidebarToggle").on("click", function(e) {
-            e.preventDefault();
-            $("body").toggleClass("sb-sidenav-toggled");
-        });
-    })(jQuery);
-  }
 }
