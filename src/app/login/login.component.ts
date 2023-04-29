bf6ca285e10a93aea841f502fe703909f262fc38
Jaime <santos.jaimemendes91@gmail.com>
2023-04-29 10:24:46 -0300

    adicionado o componete de login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -10,11 +11,22 @@ export class LoginComponent {
   username: string = "";
   password: string = "";
   loginError: boolean = false;
+  cadastrando: boolean = false;
 
-  constructor () {}
+  constructor (
+    private router: Router
+  ) {}
 
   onSubmit() {
-    console.log(`User: ${this.username}, Pass: ${this.password}`)
+    this.router.navigate(['/home'])
   }
 
+  preparaCadastrar(event: any) {
+    event.preventDefault();
+    this.cadastrando = true;
+  }
+
+  cancelaCadastro() {
+    this.cadastrando = false;
+  }
 }
