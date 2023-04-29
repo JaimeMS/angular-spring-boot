1e3c38aaffbf90f0c03ffd0c148e4f8b88b20e30
Jaime <santos.jaimemendes91@gmail.com>
2023-04-29 11:27:35 -0300

    refatorada as rotas do sistema

--- src/app/clientes/clientes-form/clientes-form.component.ts
+++ src/app/clientes/clientes-form/clientes-form.component.ts
@@ -60,7 +60,7 @@ export class ClientesFormComponent implements OnInit{
   }
 
   voltarParaListagem() {
-    this.router.navigate(['/clientes-lista']);
+    this.router.navigate(['/clientes/lista']);
   }
 
 }
